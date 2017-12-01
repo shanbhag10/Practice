8b49a4aa2f1be5ff95b0203409e0c598fba59650
Saurabh Shanbhag <sshanbh2@ncsu.edu>
2017-12-01 04:11:29 -0500

    Update longest_cmn_prefix.java

--- longest_cmn_prefix.java
+++ longest_cmn_prefix.java
@@ -1,4 +1,26 @@
-class Solution {
+class Main {
+  
+  
+    public static void main(String[] args){
+      
+        String[] arr = {"Sarubh","Sarubha","Sarubnh","Saras"};
+        
+        System.out.println(longestCommonPrefix(arr));
+        
+        System.out.println(lcp(arr,0));
+    }
+    
+
+    public static int smallest(String[] strs){
+      int min=Integer.MAX_VALUE;
+      for(int i=0;i<strs.length;i++){
+        if(strs[i].length()<min)
+          min=strs[i].length();
+      }
+      return min;
+    }
+    
+    
     public static String longestCommonPrefix(String[] strs) {
        
        String s="";
@@ -9,7 +31,7 @@ class Solution {
       }
        int i;
        for(i=0;i<min;i++){
-         char c=strs[1].charAt(i);
+         char c=strs[0].charAt(i);
          boolean b=false;
          for(int j=0;j<strs.length;j++){
            
@@ -29,3 +51,8 @@ class Solution {
         return s;
 }
 }
+
+/*
+S a r u b h 
+S a r u h
+S a r u n h*/
